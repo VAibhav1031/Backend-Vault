a902351172cfb9699248bd91d103f62c5f28098e
Vaibhav Tiwari <vaibhavgrand2004@gmail.com>
2025-06-19 16:36:09 +0530

    User Authentication: Added User Authentication,used the bcrypt for hashing password, created our own  validation for register,  use built it in LoginManager for login thing

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "df81e3d4e2916edb1677ad7854d6f84e"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from flaskblog import routes  # noaq
