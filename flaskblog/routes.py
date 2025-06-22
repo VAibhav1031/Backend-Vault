4d2a80cc1f64c877313ff1d709bd94f445f36bc0
Vaibhav Tiwari <vaibhavgrand2004@gmail.com>
2025-06-22 19:15:12 +0530

    Created page for adding the new post and updating the database with changes

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -3,30 +3,15 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-
-
-posts = [
-    {
-        "author": "Vaibhav Tiwari",
-        "title": "Blog Post 1",
-        "content": "First Post content",
-        "date_posted": "April 21, 2025",
-    },
-    {
-        "author": "Corey scaffer",
-        "title": "Blog Post 2",
-        "content": "Second Post content",
-        "date_posted": "May 5, 2025",
-    },
-]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template("home.html", posts=posts)
 
 
@@ -81,7 +66,8 @@ def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, "static/profile_pics", picture_fn)
+    picture_path = os.path.join(
+        app.root_path, "static/profile_pics", picture_fn)
     output_size = (205, 205)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
@@ -107,7 +93,23 @@ def account():
     elif request.method == "GET":  # will  retrieve the values and populate the sections
         form.username.data = current_user.username
         form.email.data = current_user.email
-    image_file = url_for("static", filename="profile_pics/" + current_user.image_file)
+    image_file = url_for(
+        "static", filename="profile_pics/" + current_user.image_file)
     return render_template(
         "account.html", title="Account", image_file=image_file, form=form
     )
+
+
+@app.route("/post/new", methods=["GET", "POST"])
+@login_required
+def new():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(
+            title=form.title.data, content=form.content.data, author=current_user
+        )
+        db.session.add(post)
+        db.session.commit()
+        flash("You have successfully created the post!!", "success")
+        return redirect("home")
+    return render_template("create_post.html", title="New Post", form=form)
