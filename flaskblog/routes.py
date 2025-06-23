51bb8ab33f7741159dc492bb3bb3c941979b069b
Vaibhav Tiwari <vaibhavgrand2004@gmail.com>
2025-06-23 15:58:36 +0530

    Now we can update , delete the post , we  also  use the Bootstrap MODAL for  confirmation before deletion

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
@@ -100,10 +100,13 @@ def account():
     )
 
 
-@app.route("/post/new", methods=["GET", "POST"])
+@app.route("/post/new/", methods=["GET", "POST"])
 @login_required
 def new():
-    form = PostForm()
+    print("REQUEST METHOD:", request.method)
+    print("REQUEST PATH:", request.path)
+
+    form = PostForm()  # this is  where  we  create  the form , and  we  access this  in the  create_post.html  through code brackets and
     if form.validate_on_submit():
         post = Post(
             title=form.title.data, content=form.content.data, author=current_user
@@ -111,5 +114,49 @@ def new():
         db.session.add(post)
         db.session.commit()
         flash("You have successfully created the post!!", "success")
-        return redirect("home")
-    return render_template("create_post.html", title="New Post", form=form)
+        return redirect(url_for("home"))  # <-- this part
+    return render_template(
+        "create_post.html", title="New Post", form=form, legend="New Post"
+    )
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template("post.html", title=post.title, post=post)
+
+
+# in this  dont be  confused  ,we  are mainly doing what is  populating during the GET request and
+# if we have to update and  we will  update the db , why we  are not  adding the db.session  cause
+# it already been created , we  are  just  changing ,
+@app.route("/post/<int:post_id>/update", methods=["GET", "POST"])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash("Your post has been updated!", "success")
+        return redirect(url_for("post", post_id=post.id))
+    elif request.method == "GET":
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template(
+        "create_post.html", title="Update Post", form=form, legend="Update Post"
+    )
+
+
+@app.route("/post/<int:post_id>/delete", methods=["POST"])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash("Your post has been deleted!", "success")
+    return redirect(url_for("home"))
