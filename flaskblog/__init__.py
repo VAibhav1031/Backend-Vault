c3604f7631ccbf219851434de4c6e3b1a8b36d74
Vaibhav Tiwari <vaibhavgrand2004@gmail.com>
2025-07-03 17:40:33 +0530

    Update for reseting password by sending link  to the email

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,15 +1,31 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # load environment variables from .env
+
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "df81e3d4e2916edb1677ad7854d6f84e"
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "fallback_key")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
 
-from flaskblog import routes  # noaq
+# Mail Config
+app.config["MAIL_SERVER"] = "smtp.googlemail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.environ.get("EMAIL_USER")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+
+mail = Mail(app)
+
+from flaskblog import routes
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from flaskblog import db, login_manager
+from itsdangerous import URLSafeTimedSerializer as Serializer
+from flaskblog import db, login_manager, app
 from flask_login import UserMixin
 
 
@@ -12,14 +13,28 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False,
-                           default="default.jpg")
+    image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
     password = db.Column(
         db.String(60),
         nullable=False,
     )
 
     posts = db.relationship("Post", backref="author", lazy=True)
+
+    def get_reset_token(self):
+        # .decode("utf-8") no decode required in new URLSafeTimedSerializer
+        s = Serializer(app.config["SECRET_KEY"])
+        return s.dumps({"user_id": self.id}, salt="password-reset")
+
+    @staticmethod
+    def verify_reset_token(token, expires_sec=1800):
+        s = Serializer(app.config["SECRET_KEY"])
+        try:
+            data = s.loads(token, salt="password-reset", max_age=expires_sec)
+            user_id = data["user_id"]
+        except:
+            return None
+        return User.query.get(user_id)
 
     def __repr__(self):
         return f"User('{self.username}','{self.email}','{self.image_file}','{self.password}')"
@@ -28,8 +43,7 @@ class User(db.Model, UserMixin):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False,
-                            default=datetime.utcnow)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
