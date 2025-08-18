7040072988dd22a9aa028ef5e86175f748de7da0
Vaibhav Tiwari <vaibhavgrand2004@gmail.com>
2025-08-19 00:11:53 +0530

    LITTLE-Fix:import error fixed

--- Flask_Task_Manager/flask_task_manager/schemas.py
+++ Flask_Task_Manager/flask_task_manager/schemas.py
@@ -1,12 +1,12 @@
-from marshmallow import schemas, fields, ValidationError
+from marshmallow import Schema, fields, ValidationError
 
 
-class UserSchema(schemas):
+class UserSchema(Schema):
     username = fields.Str(required=True)
     email = fields.Str(required=True)
     password = fields.Str(required=True)
 
 
-class AddTask(schemas):
+class AddTask(Schema):
     title = fields.Str(required=True)
     description = fields.Str(required=True)
