0bd51ebb516bd832199bdf770eb569b22d1d6009
Vaibhav Tiwari <vaibhavgrand2004@gmail.com>
2025-08-27 01:20:34 +0530

    Improved Mail-Msg: Added HTML and Normal txt based option for mail message

--- Flask_Task_Manager/flask_task_manager/utils.py
+++ Flask_Task_Manager/flask_task_manager/utils.py
@@ -1,6 +1,6 @@
 import datetime
 from functools import wraps
-from flask import current_app
+from flask import current_app, render_template
 import jwt
 from flask import request
 from .error_handler import unauthorized_error, too_many_requests, bad_request
@@ -189,13 +189,24 @@ def otp_generator():
     return str(secrets.randbelow(10**6)).zfill(6)
 
 
+# def send_reset_email(user, otp):
+#     msg = Message(
+#         "Password reset Request", sender="noreply@demo.com", recipients=[user.email]
+#     )
+#     msg.body = f"""To reset your password, Here it is yout OTP:
+# {otp}
+#     Hurry up , you got 1 min / 60 sec to validate
+# If you did not make this request then simply ignore this email and no changes will be made
+# """
+#     mail.send(msg)
+
+
 def send_reset_email(user, otp):
     msg = Message(
         "Password reset Request", sender="noreply@demo.com", recipients=[user.email]
     )
-    msg.body = f"""To reset your password, Here it is yout OTP:
-{otp}
-    Hurry up , you got 1 min / 60 sec to validate
-If you did not make this request then simply ignore this email and no changes will be made
-"""
+    msg.body = render_template(
+        "reset_password.txt", username=user.username, OTP=otp)
+    msg.html = render_template(
+        "reset_password.html", username=user.username, OTP=otp)
     mail.send(msg)
